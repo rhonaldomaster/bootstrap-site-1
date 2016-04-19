6c9425d19da16c22c5b63b4c1e1c49deef7d6ee5
rhonaldomaster <rhonaldomaster@gmail.com>
2016-04-19 15:33:27 -0500

    mejoras varias

--- js/app.js
+++ js/app.js
@@ -81,52 +81,7 @@ var myapp = (function () {
 })();
 $(document).ready(function () {
   myapp.init();
-  $("table").tablesorter({
-    // this will apply the bootstrap theme if "uitheme" widget is included
-    // the widgetOptions.uitheme is no longer required to be set
-    theme : "bootstrap",
-
-    widthFixed: true,
-
-    headerTemplate : '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon!
-
-    // widget code contained in the jquery.tablesorter.widgets.js file
-    // use the zebra stripe widget if you plan on hiding any rows (filter widget)
-    widgets : [ "uitheme", "filter", "zebra" ],
-
-    widgetOptions : {
-      // using the default zebra striping class name, so it actually isn't included in the theme variable above
-      // this is ONLY needed for bootstrap theming if you are using the filter widget, because rows are hidden
-      zebra : ["even", "odd"],
-
-      // reset filters button
-      filter_reset : ".reset",
-
-      // extra css class name (string or array) added to the filter element (input or select)
-      filter_cssFilter: "form-control",
-
-      // set the uitheme widget to use the bootstrap theme class names
-      // this is no longer required, if theme is set
-      // ,uitheme : "bootstrap"
-
-    }
-  })
-  .tablesorterPager({
-
-    // target the pager markup - see the HTML block below
-    container: $(".ts-pager"),
-
-    // target the pager page select dropdown - choose a page
-    cssGoto  : ".pagenum",
-
-    // remove rows from the table to speed up the sort of large tables.
-    // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
-    removeRows: false,
-
-    // output string - default is '{page}/{totalPages}';
-    // possible variables: {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
-    output: '{startRow} - {endRow} / {filteredRows} ({totalRows})'
-
+  $(".js-showEditModal").click(function(){
+    $(".js-editModal").modal();
   });
-
 });
